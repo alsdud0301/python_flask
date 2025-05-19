72ef430f79eb56610757d5b79aa5fdc9e6faea2d
alsdud0301 <jm931017@gmail.com>
2025-05-20 01:54:13 +0900

    메모 생성, 조회, 수정, 삭제

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, abort, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
@@ -24,5 +24,41 @@ def about():
     return '이것은 마이 메모 앱의 소개 페이지입니다.'
 
 # 데이터베이스 생성
-with app.app_context:
+with app.app_context():
     db.create_all()
+    
+# 메모 생성
+@app.route('/memos/create',methods=['POST'])
+def create_memo():
+    title = request.json['title']
+    content = request.json['content']
+    new_memo = Memo(title=title,content=content)
+    db.session.add(new_memo)
+    db.session.commit()
+    return jsonify({'message': 'Memo created'}),201
+
+@app.route('/memos',methods=['GET'])
+def list_memos():
+    memos = Memo.query.all()
+    return jsonify([{'id':memo.id, 'title': memo.title,'content': memo.content} for memo in memos]),200
+
+@app.route('/memos/update/<int:id>', methods=['PUT'])
+def update_memo(id):
+    memo = Memo.query.filter_by(id=id).first()
+    if memo:
+        memo.title=request.json['title']
+        memo.content = request.json['content']
+        db.session.commit()
+        return jsonify({'message':'Memo updated'}),200
+    else:
+        abort(404,description="memo not found")
+
+@app.route('/memos/delete/<int:id>', methods=['DELETE'])
+def delete_memo(id):
+    memo = Memo.query.filter_by(id=id).first()
+    if memo:
+        db.session.delete(memo)
+        db.session.commit()
+        return jsonify({'message': 'Memo deleted'}),200
+    else:
+        abort(404,description="memo not found")
